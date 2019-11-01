1736a510fc2492d9bda5592c791142df1a76d8b3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-01 13:14:54 +0200

    init

--- HowScrollViewLetsUsWorkWithScrollingData/ContentView.swift
+++ HowScrollViewLetsUsWorkWithScrollingData/ContentView.swift
@@ -8,9 +8,30 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ScrollView(.vertical) {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
